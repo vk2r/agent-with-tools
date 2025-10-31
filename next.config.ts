e9f854d9fe975e4ae9c33811666cae638ebd3d07
Victor Mansilla (vk3r) <theadwareman@gmail.com>
2025-10-31 17:59:56 -0300

    🧱 update configuration and dependencies

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["@mastra/*"],
+  reactCompiler: true,
+  experimental: {
+    viewTransition: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
