0b52845b027b0da1277f4ef32cfd5fdcd950218c
Victor Mansilla (vk3r) <theadwareman@gmail.com>
2025-11-17 17:50:20 -0300

    ♻️ refactor use-in-view hook import and syntax

--- src/hooks/use-is-in-view.tsx
+++ src/hooks/use-is-in-view.tsx
@@ -1,17 +1,16 @@
-import * as React from 'react';
-import { useInView, type UseInViewOptions } from 'motion/react';
+import { type UseInViewOptions, useInView } from "motion/react";
 
 interface UseIsInViewOptions {
   inView?: boolean;
   inViewOnce?: boolean;
-  inViewMargin?: UseInViewOptions['margin'];
+  inViewMargin?: UseInViewOptions["margin"];
 }
 
 function useIsInView<T extends HTMLElement = HTMLElement>(
   ref: React.Ref<T>,
   options: UseIsInViewOptions = {},
 ) {
-  const { inView, inViewOnce = false, inViewMargin = '0px' } = options;
+  const { inView, inViewOnce = false, inViewMargin = "0px" } = options;
   const localRef = React.useRef<T>(null);
   React.useImperativeHandle(ref, () => localRef.current as T);
   const inViewResult = useInView(localRef, {
